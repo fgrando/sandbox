27d88790dae7408a4e715024b663f9e3f4677124
fgrando <fernandozatt@gmail.com>
2024-07-23 23:51:22 +0200

    add example

---
+++ plugin-app/plugins/diff_html.py
@@ -0,0 +1,10 @@
+import plugins
+
+
+class Diff(plugins.Base):
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        print("Diff Plugin")
---
+++ plugin-app/plugins/inspect.py
@@ -0,0 +1,10 @@
+import plugins
+
+
+class Inspector(plugins.Base):
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        print("Inspect Plugin")
